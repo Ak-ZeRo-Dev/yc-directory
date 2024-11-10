c4975f4cb79650699f70e21fa3c3f35d970bfb7a
Abdulrahman Alkurdi <113478579+Ak-ZeRo-Dev@users.noreply.github.com>
2024-11-10 21:03:05 +0200

    Update page.tsx

--- src/app/(root)/startup/[id]/page.tsx
+++ src/app/(root)/startup/[id]/page.tsx
@@ -18,27 +18,25 @@ const md = markdownit();
 
 export const experimental_ppr = true;
 
-export default async function page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {  
-  const { id } = await params;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
 
   const [post, { select: editorPosts }] = await Promise.all([
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
-    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks-new" }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
   ]);
 
   if (!post) return notFound();
 
-  const editorPosts = editorData?.select || [];
   const parsedContent = md.render(post?.pitch || "");
 
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <p className="tag">{formatDate(post?._createdAt)}</p>
+
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
@@ -63,6 +61,7 @@ export default async function page({
                 height={64}
                 className="rounded-full drop-shadow-lg"
               />
+
               <div>
                 <p className="text-20-medium">{post.author.name}</p>
                 <p className="text-16-medium !text-black-300">
@@ -70,6 +69,7 @@ export default async function page({
                 </p>
               </div>
             </Link>
+
             <p className="category-tag">{post.category}</p>
           </div>
 
@@ -86,9 +86,10 @@ export default async function page({
 
         <hr className="divider" />
 
-        {editorPosts.length > 0 && (
+        {editorPosts?.length > 0 && (
           <div className="max-w-4xl mx-auto">
             <p className="text-30-semibold">Editor Picks</p>
+
             <ul className="mt-7 card_grid-sm">
               {editorPosts.map((post: StartupTypeCard, i: number) => (
                 <StartupCard key={i} post={post} />
@@ -104,3 +105,5 @@ export default async function page({
     </>
   );
 };
+
+export default Page;
